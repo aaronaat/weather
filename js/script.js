4e62dfd33ee357ad470350f99ef49d1a2db7da39
Aaron Taylor <aarontaylor@gmail.com>
2019-02-05 13:27:40 -0800

    add some style and html, work on javascript backend, add img folder

--- js/script.js
+++ js/script.js
@@ -4,14 +4,33 @@ function Forecast() {
   this.wind = 0;
 }
 Forecast.prototype.ranTemp = function() {
-  this.temp = Math.floor(Math.random() * 62) + 28;
+  this.temp = Math.floor((Math.random() * (62 - 28) + 28));
 }
 Forecast.prototype.ranPrecip = function() {
-
+  this.precip = Math.floor((Math.random() * 100) + 1);
 }
 Forecast.prototype.ranWind = function() {
-
+  this.wind = Math.floor((Math.random() * 9) + 1);
 }
-
+Forecast.prototype.reset = function() {
+  this.temp = 0;
+  this.precip = 0;
+  this.wind = 0;
+}
 var monday = new Forecast();
-monday.ranTemp();
+
+
+$(document).ready(function(){
+
+  $( "#cast" ).click(function() {
+    monday.reset();
+    monday.ranTemp();
+    monday.ranPrecip();
+    monday.ranWind();
+    alert(monday.temp);
+    alert(monday.precip);
+    alert(monday.wind);
+
+  });
+
+});
