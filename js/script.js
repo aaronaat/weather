3e4fc9158be8fd87f777fecf51839e0aa0aa5f8c
Aaron Taylor <aarontaylor@gmail.com>
2019-02-05 15:18:41 -0800

    work on prototype to set image

--- js/script.js
+++ js/script.js
@@ -2,23 +2,31 @@ function Forecast() {
   this.temp = 0;
   this.precip = 0;
   this.wind = 0;
-}
+};
 Forecast.prototype.ranTemp = function() {
   this.temp = Math.floor((Math.random() * (62 - 28) + 28));
-}
+};
 Forecast.prototype.ranPrecip = function() {
   this.precip = Math.floor((Math.random() * 100) + 1);
-}
+};
 Forecast.prototype.ranWind = function() {
   this.wind = Math.floor((Math.random() * 9) + 1);
-}
+};
 Forecast.prototype.reset = function() {
   this.temp = 0;
   this.precip = 0;
   this.wind = 0;
-}
+};
+Forecast.prototype.setImg = function() {
+  if ((this.temp <= 35) && (this.precip >= 49)) {
+      $("#mondayImg").attr("src", "img/snow-rain.png");
+  };
+};
 var monday = new Forecast();
-
+var tuesday = new Forecast();
+var wednesday = new Forecast();
+var thursday = new Forecast();
+var friday = new Forecast();
 
 $(document).ready(function(){
 
@@ -27,9 +35,10 @@ $(document).ready(function(){
     monday.ranTemp();
     monday.ranPrecip();
     monday.ranWind();
-    alert(monday.temp);
-    alert(monday.precip);
-    alert(monday.wind);
+    $("#mondayTemp").text("Temp: " + monday.temp + "f");
+    $("#mondayPrecip").text("Precip: " + monday.precip + "%");
+    $("#mondayWind").text("Wind speed: " + monday.wind + "mph");
+    monday.setImg();
 
   });
 
