6623e3e6759cd2925857f2739594b182fa24ad54
Aaron Taylor <aarontaylor@gmail.com>
2019-02-05 15:54:34 -0800

    dry up code, finish logic to set image

--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
-function Forecast() {
+function Forecast(day) {
   this.temp = 0;
   this.precip = 0;
   this.wind = 0;
+  this.day = day;
 };
 Forecast.prototype.ranTemp = function() {
   this.temp = Math.floor((Math.random() * (62 - 28) + 28));
+  $("#" + this.day + "Temp").text("Temp: " + this.temp + "f");
 };
 Forecast.prototype.ranPrecip = function() {
   this.precip = Math.floor((Math.random() * 100) + 1);
+  $("#" + this.day + "Precip").text("Precip: " + this.precip + "%");
 };
 Forecast.prototype.ranWind = function() {
   this.wind = Math.floor((Math.random() * 9) + 1);
+  $("#" + this.day + "Wind").text("Wind speed: " + monday.wind + "mph");
 };
 Forecast.prototype.reset = function() {
   this.temp = 0;
@@ -19,14 +23,21 @@ Forecast.prototype.reset = function() {
 };
 Forecast.prototype.setImg = function() {
   if ((this.temp <= 35) && (this.precip >= 49)) {
-      $("#mondayImg").attr("src", "img/snow-rain.png");
+      $("#" + this.day + "Img").attr("src", "img/snow-rain.png");
+  } else if ((this.temp >= 35) && (this.precip >= 49)) {
+    $("#" + this.day + "Img").attr("src", "img/rain-cloudy.png");
+  } else if ((this.precip >= 25) && (this.precip <= 48)) {
+    $("#" + this.day + "Img").attr("src", "img/partly-cloudy.png");
+  }  else {
+    $("#" + this.day + "Img").attr("src", "img/sunny.png");
   };
 };
-var monday = new Forecast();
-var tuesday = new Forecast();
-var wednesday = new Forecast();
-var thursday = new Forecast();
-var friday = new Forecast();
+
+var monday = new Forecast("monday");
+var tuesday = new Forecast("tuesday");
+var wednesday = new Forecast("wednesday");
+var thursday = new Forecast("thursday");
+var friday = new Forecast("friday");
 
 $(document).ready(function(){
 
@@ -35,11 +46,7 @@ $(document).ready(function(){
     monday.ranTemp();
     monday.ranPrecip();
     monday.ranWind();
-    $("#mondayTemp").text("Temp: " + monday.temp + "f");
-    $("#mondayPrecip").text("Precip: " + monday.precip + "%");
-    $("#mondayWind").text("Wind speed: " + monday.wind + "mph");
     monday.setImg();
-
   });
 
 });
